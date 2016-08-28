5b7dcb8e19e28fd25bcfd4aff0a19ed59f1da3de
CLARA <CLARA@CLARA-PC>
2016-08-28 11:21:08 -0500

    new commit

---
+++ src/traductor/Traductor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package traductor;
+
+/**
+ *
+ * @author CLARA
+ */
+public class Traductor {
+    
+    
+    T_espanol_otro(int traducido){
+        //if(){}if(){}if(){}
+    }
+    void Selector(int idioma,int traducido){
+            if(idioma == 1){
+                T_espanol_otro(traducido);
+            }
+            if(){}
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
